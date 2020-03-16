7f5fd4301f297733502cbea5d4b3691f868afdc5
codebrek.com <support@codebrek.com>
2020-03-16 05:03:03 +0300

    ffirst code

---
+++ codenews_wordpress_client/lib/colors/mycolors.dart
@@ -0,0 +1,29 @@
+
+
+
+
+import 'dart:ui';
+
+class maincolors{
+
+
+  Color maincolor = HexColor("02C66B");
+  Color sec_color = HexColor("e2f2e4");
+
+
+
+
+}
+
+
+class HexColor extends Color {
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll("#", "");
+    if (hexColor.length == 6) {
+      hexColor = "FF" + hexColor;
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+}
